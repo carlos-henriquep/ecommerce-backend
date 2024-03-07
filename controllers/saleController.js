754a1eec625d85a33a582db6cbf45f41d80e7034
carlloshen <krlos.henrique98@gmail.com>
2024-03-07 16:38:07 -0300

    Feat - autentication jwt

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -2,7 +2,7 @@ import saleService from "../service/saleService.js"
 
 
 const allSaleOfUser = async(req,res)=>{
-    const {id} = req.params
+    const {id} = req.user
     const {
         errorMessage,
         statusCode,
@@ -15,13 +15,13 @@ const allSaleOfUser = async(req,res)=>{
 }
 
 const performSale = async(req,res)=>{
-    const idUser = "ce7e48e2-e4b0-489e-bc4c-bc454651593c"
+    const {id} = req.user
     const products = req.body.products
     const {
         errorMessage,
         statusCode,
         value
-    } = await saleService.performSale(products, idUser)
+    } = await saleService.performSale(products, id)
 
     const sale = errorMessage ? {errorMessage: errorMessage} : {sale: value}
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,51 @@
 import userService from "../service/userService.js";
+import jwt from "jsonwebtoken";
 
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
 
-const userLogin = async(req, res)=>{
-    const {email, password} = req.body
+  const { errorMessage, statusCode, value } = await userService.userLogin(
+    email,
+    password
+  );
 
-    const{
-        errorMessage,
-        statusCode,
-        value
-    } = await userService.userLogin(email, password)
+  const credentials = errorMessage ? { errorMessage: errorMessage } : value;
 
-    const credentials = errorMessage ? {errorMessage: errorMessage} : value
+  return res.status(statusCode).json(credentials);
+};
 
-    return res.status(statusCode).json(credentials)
-}
+const userRegister = async (req, res) => {
+  const { username, email, password } = req.body;
 
-const userRegister = async(req, res)=>{
-    const {username, email, password} = req.body
+  const { errorMessage, statusCode, value } = await userService.userRegister(
+    username,
+    email,
+    password
+  );
 
-    const {
-        errorMessage,
-        statusCode,
-        value
-    } = await userService.userRegister(username, email, password)
+  const newCredentials = errorMessage
+    ? { errorMessage: errorMessage }
+    : { message: value };
+  return res.status(statusCode).json(newCredentials);
+};
 
-    const newCredentials = errorMessage ? {errorMessage: errorMessage} : {message: value}
-    return res.status(statusCode).json(newCredentials)
+const authentication = (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+    if (!header || !header.includes("Bearer")) {
+      throw new Error("invalid token.");
+    }
 
-}
+    const token = header.split(" ")[1];
 
-const userController = {userLogin, userRegister}
+    const payload = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(401).json({ errorMessage: error.message });
+  }
+};
 
-export default userController
+const userController = { userLogin, userRegister, authentication };
+
+export default userController;
